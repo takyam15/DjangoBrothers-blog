37e3ae8d5ea4efac3b1079a40cf301f15d9264d4
takyam15 <u779052i@alumni.osaka-u.ac.jp>
2020-06-18 13:38:17 +0900

    Added the Blog model

--- blogs/models.py
+++ blogs/models.py
@@ -6,3 +6,13 @@ from django.db import models
 
 class Author(AbstractUser):
     pass
+
+
+class Blog(models.Model):
+    title = models.CharField('タイトル', blank=False, null=False, max_length=150)
+    text = models.TextField('本文', blank=True)
+    created_datetime = models.DateTimeField('作成日', auto_now_add=True)
+    updated_datetime = models.DateTimeField('更新日', auto_now=True)
+
+    def __str__(self):
+        return self.title
