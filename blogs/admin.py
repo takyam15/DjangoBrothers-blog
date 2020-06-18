0270bed3daf5467af7239b0deda748704580cef9
takyam15 <u779052i@alumni.osaka-u.ac.jp>
2020-06-18 13:38:17 +0900

    Added the Blog model

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 
-from .models import Author
+from .models import (
+    Author, Blog,
+)
 
 
 # Register your models here.
 
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_datetime', 'updated_datetime')
+    list_display_links = ('id', 'title')
+
+
 admin.site.register(Author)
+admin.site.register(Blog, BlogAdmin)
