28c3381dea3fb0e2230cfced10eb8e7f6783bf0c
takyam15 <u779052i@alumni.osaka-u.ac.jp>
2020-06-19 16:40:57 +0900

    Added test_get

--- blogs/tests.py
+++ blogs/tests.py
@@ -1,3 +1,31 @@
+import factory
 from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
+
+from .models import Blog
+
 
 # Create your tests here.
+
+class BlogFactory(factory.django.DjangoModelFactory):
+    """Create data for the Blog model used for tests
+    """
+    title = 'Example title'
+    slug = 'example-slug'
+    text = 'This is an example text. '
+    created_datetime = timezone.now()
+    updated_datetime = timezone.now()
+
+    class Meta:
+        model = Blog
+
+
+class TestIndex(TestCase):
+    def test_get(self):
+        blog_1 = BlogFactory()
+        blog_2 = BlogFactory()
+        res = self.client.get(reverse('blogs:index'))
+        blogs = res.context['blogs']
+        self.assertTemplateUsed(res, 'blogs/index.html')
+        self.assertEqual(len(blogs), 2)
