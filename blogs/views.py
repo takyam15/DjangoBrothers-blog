cddaa585cc2cac366dfece6844abf00b460e60c8
takyam15 <u779052i@alumni.osaka-u.ac.jp>
2020-06-19 14:18:48 +0900

    Created basic urls, views, and templates

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from .models import Blog
+
+
 # Create your views here.
+
+def index(request):
+    blogs = Blog.objects.order_by('-created_datetime')
+    context = {'blogs': blogs}
+    return render(request, 'blogs/index.html', context)
+
+
+def detail(request, blog_id):
+    blog = Blog.objects.get(id=blog_id)
+    context = {'blog': blog}
+    return render(request, 'blogs/detail.html', context)
